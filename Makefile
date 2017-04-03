2961abf42d2193a148be5b5902c10220a77b03a0
Rafael Henrique Tibaes <r@fael.nl>
2017-04-03 15:56:58 -0300

    warming up

---
+++ Makefile
@@ -0,0 +1,1 @@
+CXXFLAGS = -O2 --std=c++11 -Wall `pkg-config --cflags --libs opencv`
---
+++ cohelines.cpp
@@ -0,0 +1,7 @@
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include <iostream>
+
+int main() { return 0; }
